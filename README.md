c4cf3eae20bf46243d83c5c38ac6d75f724226b6
Kenta Wakasa <wakasa.kenta.pg@gmail.com>
2022-04-10 15:53:38 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# ichigyo
+
+メモを最速で書ける。一行メモアプリ。
+
+## アイコンの生成
+
+`flutter pub run flutter_launcher_icons:main`
---
+++ android/app/src/main/kotlin/com/pressedkonbu/ichigyo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.pressedkonbu.ichigyo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
